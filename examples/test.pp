38e7cc81f619b62fcae7f085acf1a2a6e3f3ffc6
Gjermund Jensvoll <sgnl05@users.noreply.github.com>
2016-02-01 19:26:08 +0100

    Black on green background

--- examples/test.pp
+++ examples/test.pp
@@ -1,7 +1,7 @@
 class { 'colorprompt':
   env_name          => 'TEST',
-  env_color         => ['white', 'bg_green'],
-  server_color      => 'white',
+  env_color         => ['bright', 'black', 'bg_green'],
+  server_color      => ['birght', 'green'],
   default_usercolor => 'cyan',
   custom_usercolors => {
     'root' => 'magenta',
