4c9773a15bc271d461079d4419aeef014f6bc07e
sgnl05 <gjerjens@gmail.com>
2016-02-01 20:50:41 +0100

    Fix colors

--- examples/test.pp
+++ examples/test.pp
@@ -1,7 +1,7 @@
 class { 'colorprompt':
   env_name          => 'TEST',
-  env_color         => ['bright', 'black', 'bg_green'],
-  server_color      => ['birght', 'green'],
+  env_color         => ['black', 'bg_green'],
+  server_color      => ['bright', 'green'],
   default_usercolor => 'cyan',
   custom_usercolors => {
     'root' => 'magenta',
